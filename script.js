1e71b045cc01b9e45dced3c52e88c980ad5db1f6
ikonikisop <129855365+ikonikisop@users.noreply.github.com>
2025-01-28 20:22:06 -0600

    added password eye

---
+++ script.js
@@ -0,0 +1,16 @@
+// Toggle Password Visibility
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const eyeIcon = document.querySelector('.eye-icon i');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    }
+}
